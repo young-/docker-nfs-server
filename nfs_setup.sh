7bbf1eccd0dbda0461b4986f0f75125d02feba79
pandrew <letters@paulnotcom.se>
2014-05-27 23:48:03 +0200

    specify mounts via --env-file

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -5,12 +5,21 @@ set -e
 mounts="${@}"
 
 if [[ -z $PARAMS ]]; then
-        PARAMS="*(rw,sync,no_subtree_check,fsid=0,no_root_squash)"
+  PARAMS="*(rw,sync,no_subtree_check,fsid=0,no_root_squash)"
 fi
+
+# Specify mounts via --env-file using a list of EXPORT variables
+# EXPORT1="/export 10.0.0.1(ro,sync,no_subtree_check,insecure)"
+# EXPORT2="/path/to/volume 10.0.0.2(rw,sync,no_subtree_check,insecure)"
+for v in EXPORT{1..100}; do 
+  echo "${!v}" >> /etc/exports
+done
 
 for mnt in "${mounts[@]}"; do
   src=$(echo $mnt | awk -F':' '{ print $1 }')
   echo "$src $PARAMS" >> /etc/exports
 done
+
+
 
 exec runsvdir /etc/sv
