bd1e94b09f6e76f5f458a8aef7bba14929637359
Paul Andrew Liljenberg <letters@paulnotcom.se>
2014-05-27 15:19:04 +0200

    set nfs parameters dynamically

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -4,9 +4,13 @@ set -e
 
 mounts="${@}"
 
+if [[ -z $PARAMS ]]; then
+        PARAMS="*(rw,sync,no_subtree_check,fsid=0,no_root_squash)"
+fi
+
 for mnt in "${mounts[@]}"; do
   src=$(echo $mnt | awk -F':' '{ print $1 }')
-  echo "$src *(rw,sync,no_subtree_check,fsid=0,no_root_squash)" >> /etc/exports
+  echo "$src $PARAMS" >> /etc/exports
 done
 
 exec runsvdir /etc/sv
