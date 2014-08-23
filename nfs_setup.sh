304bd8f106e764226cd78bbeeade2ba828ba5f9f
Paul Andrew Liljenberg <liljenberg.paul@gmail.com>
2014-08-23 20:30:47 +0200

    ensure only nonempty vars is added to /etc/exports

--- nfs_setup.sh
+++ nfs_setup.sh
@@ -9,8 +9,10 @@ PARAMS="${PARAMS:-*(rw,sync,no_root_squash,no_subtree_check,insecure)}"
 # Specify mounts via --env-file using a list of EXPORT variables
 # EXPORT1="/export 10.0.0.1(ro,sync,no_subtree_check,insecure)"
 # EXPORT2="/path/to/volume 10.0.0.2(rw,sync,no_subtree_check,insecure)"
-for v in EXPORT{1..100}; do 
-  echo "${!v}" >> /etc/exports
+for EXPORTS in EXPORT{1..100}; do
+  if [[ ! -z ${!EXPORTS} ]]; then
+    echo "${!EXPORTS}" >> /etc/exports
+  fi
 done
 
 for mnt in "${mounts[@]}"; do
